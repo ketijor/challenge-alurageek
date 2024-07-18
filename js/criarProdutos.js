aa68da21373a7370ea279a4ea7b657d35af70c58
ketijor <keeeti_@hotmail.com>
2024-07-18 11:42:41 -0300

    Ajustes

---
+++ js/criarProdutos.js
@@ -0,0 +1,12 @@
+import { conectaApi } from "./scriptApi.js";
+
+const formulario = document.querySelector('[data-formulario]');
+
+async function criarProduto(evento) {
+  const nome = document.querySelector('[data-nome]').value;
+  const preco = document.querySelector('[data-preco]').value;
+  const imagem = document.querySelector('[data-imagem]').value;
+
+  await conectaApi.criaProduto(nome, preco, imagem);
+}
+formulario.addEventListener('submit', evento => criarProduto(evento));
